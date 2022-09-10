80fdd44ff66fdd05e9f1e88b9269ccea72a81d69
Anser <aj@wemove.com>
2022-09-10 18:31:29 +0200

    fix linking

--- templates/contents/three-grid.php
+++ templates/contents/three-grid.php
@@ -7,7 +7,7 @@
                         <img class="card-img-top" src="<?= $card->image->size("600", "350")->url ?>" alt="..." />
                         <div class="card-body p-4">
                             <div class="badge bg-primary bg-gradient rounded-pill mb-2"><?= $card->textfield ?></div>
-                            <a class="text-decoration-none link-dark stretched-link" href="<?= $card->menu_sites->url ?>"><h5 class="card-title mb-3"><?= $card->headline ?></h5></a>
+                            <a class="text-decoration-none link-dark stretched-link" href="<?= $card->menu_sites->first->url ?>"><h5 class="card-title mb-3"><?= $card->headline ?></h5></a>
                             <p class="card-text mb-0"><?= $card->textarea ?></p>
                         </div>
                     </div>
