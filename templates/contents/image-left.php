167be4d53654d7f985c45f34c539d4d23c83b571
Anser <aj@wemove.com>
2022-09-10 21:30:22 +0200

    align contents with max width

--- templates/contents/image-left.php
+++ templates/contents/image-left.php
@@ -1,5 +1,5 @@
 <section class="py-5 bg-light image_left content">
-    <div class="container px-5 my-5">
+    <div class="container my-5">
         <div class="row gx-5 align-items-center">
             <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="<?= $page_child->image->size("600", "400")->url ?>" alt="" /></div>
             <div class="col-lg-6">
